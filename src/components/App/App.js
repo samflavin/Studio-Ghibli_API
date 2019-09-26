752fa53755f60be0b11e9473ae9776318d4ff534
Sam Flavin <flavi022@d.umn.edu>
2019-09-26 17:19:35 -0500

    added background and cards

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import './App.css';
-import { Card, CardContent } from 'semantic-ui-react';
+import { Card, CardContent, Segment } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css'
 const axios = require('axios');
 
@@ -33,16 +33,17 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <header className="App-header">
+      <div className="main">
+        <header className="App-header" >
           <h1 align="center">Studio Ghibli API</h1>
           < br/>
         </header>
-        <Card.Group itemsPerRow={4}>
+        <Card.Group itemsPerRow={2}>
           {this.props.storeInstance.random && this.props.storeInstance.random.length > 0 ?
             this.props.storeInstance.random.map(item => {
               console.log(item)
               return (
+
                 <Card color='blue' fluid='true' raised='true'>
                   <h4>{item.title}</h4>
                   <CardContent>
@@ -55,9 +56,8 @@ class App extends Component {
           }
         </Card.Group>
        
+       
          
-
-        <button onClick={this.handleClick}>Get a Giphy</button>
       </div>
     );
   }
